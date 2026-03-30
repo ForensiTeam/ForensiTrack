c7af9cc3303849fca5410cd7674ee08dbdda0062
Yiğit Bayraktar <bayraktaryigit0@gmail.com>
2026-03-30 16:16:30 +0300

    fix(backend): Tool schema'sına eksik olan overallRating eklendi

--- backend/models/Tool.js
+++ backend/models/Tool.js
@@ -1,11 +1,12 @@
-﻿// Author: Ümmühan Atmaca - G9-G16 Gereksinimleri
+// Author: Ümmühan Atmaca - G9-G16 Gereksinimleri
 const mongoose = require('mongoose');
 
 const toolSchema = new mongoose.Schema({
   name: { type: String, required: true },
   category: { type: String, required: true, enum: ['Bellek Analizi', 'Disk Analizi', 'Ag Analizi', 'Mobil Analiz', 'Sifreleme'] },
   description: { type: String, required: true },
-  version: { type: String }
+  version: { type: String },
+  overallRating: { type: Number, default: 0 }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Tool', toolSchema);
